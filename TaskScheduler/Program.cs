19a1e49e286337684853a7a644f3ad5a1bc163d4
0144 <jamie.lin@afisc.com.tw>
2022-04-14 15:33:22 +0800

    add dll

--- TaskScheduler/Program.cs
+++ TaskScheduler/Program.cs
@@ -10,53 +10,63 @@ namespace TaskScheduler
     {
         private static void Main(string[] args)
         {
-            TaskService taskService = new TaskService();
-            TaskCollection taskCollection = taskService.GetFolder(@"\").GetTasks();
-            string[] taskName;
-
             string logName;
             string logFullPath;
-            StreamWriter log;
+            string pathNow;
+            string output;
 
-            if (Properties.Settings.Default.TaskName != null && Properties.Settings.Default.TaskName != string.Empty && taskCollection.Count != 0)
+            try
             {
-                taskName = Properties.Settings.Default.TaskName.Split(',');
-                if (taskName != null && taskName.Length != 0)
+                pathNow = Directory.GetCurrentDirectory();
+                TaskService taskService = new TaskService();
+                TaskCollection taskCollection = taskService.GetFolder(@"\").GetTasks();
+                string[] taskName;
+
+                if (Properties.Settings.Default.TaskName != null && Properties.Settings.Default.TaskName != string.Empty && taskCollection.Count != 0)
                 {
-                    string pathNow = Directory.GetCurrentDirectory();
-                    string now = DateTime.Now.ToString("yyyyMMdd");
-                    logName = now + ".log";   //以當時為名的log
-
-                    if (!System.IO.Directory.Exists("TaskScheduler_log"))
-                        System.IO.Directory.CreateDirectory("TaskScheduler_log");  //沒有資料夾就產生
-
-                    logFullPath = Path.Combine("TaskScheduler_log", logName);
-
-                    for (int i = 0; i < taskName.Length; i++)
+                    output = Properties.Settings.Default.Output.ToString().Trim();
+                    taskName = Properties.Settings.Default.TaskName.Split(',');
+                   
+                    if (taskName != null && taskName.Length != 0)
                     {
-                        for (int j = 0; j < taskCollection.Count; j++)
-                        {
-                            if (taskCollection[j].Name.Equals(taskName[i]))
-                            {
-                                if (!File.Exists(logFullPath))
-                                    log = new StreamWriter(logFullPath);
-                                else
-                                    log = File.AppendText(logFullPath);
+                        logName = "排程log.txt";   //以當時為名的log
 
-                                log.WriteLine("log產生時間{0}", DateTime.Now.ToString());
-                                log.WriteLine("排程:{0},執行狀態:{1}", taskCollection[j].Name, taskCollection[j].LastTaskResult);
-                                log.WriteLine("最後執行時間:{0},下次執行時間:{1}", taskCollection[j].LastRunTime, taskCollection[j].NextRunTime.ToString());
-                                log.WriteLine("\n");
-                                log.Flush();
-                                log.Close();
-                                log.Dispose();
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine($"排程執行時間:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+                        sb.AppendLine("");
+                        for (int i = 0; i < taskName.Length; i++)
+                        {
+                            for (int j = 0; j < taskCollection.Count; j++)
+                            {
+                                if (taskCollection[j].Name.Equals(taskName[i]))
+                                {
+                                    sb.AppendLine($"排程:{taskCollection[j].Name},執行狀態:{taskCollection[j].LastTaskResult}");
+                                    sb.AppendLine($"最後執行時間:{taskCollection[j].LastRunTime},下次執行時間:{taskCollection[j].NextRunTime.ToString()}");
+                                    sb.AppendLine("");
+                                }
                             }
+
+                            logFullPath = Path.Combine(output, logName);
+                            using (StreamWriter sw = new StreamWriter(logFullPath, false))
+                            {
+                                sw.Write(sb.ToString());
+                            }
+
                         }
                     }
+
                 }
             }
-
-            //Console.ReadKey();
+            catch (Exception ex)
+            {
+                logName = "err.txt";
+                logFullPath = Path.Combine(Properties.Settings.Default.Output.ToString().Trim(), logName);
+                using (StreamWriter sw = new StreamWriter(logFullPath, false))
+                {
+                    sw.Write(ex.ToString());
+                }
+            }
+            
         }
     }
 }
